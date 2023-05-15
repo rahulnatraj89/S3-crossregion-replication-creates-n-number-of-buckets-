877bae89393e9e3d085b42bbefb72c585df61f59
rahulnatraj89 <122457791+rahulnatraj89@users.noreply.github.com>
2023-05-15 11:46:25 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,9 @@
 output "primary" {
-    count = length(var.primary)
     value = aws_s3_bucket.primary
   description = "The primary S3 bucket"
 }
 
 output "secondary" {
-    count = length(var.primary)
   value = aws_s3_bucket.secondary
   description = "The secondary S3 bucket"
 }
